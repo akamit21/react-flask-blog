9ec243a155ae082ab79581bcd9e6a0a1a9b2f733
Amit Kumar <akamit21@outlook.com>
2020-02-27 01:45:07 +0530

    File Modified

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
