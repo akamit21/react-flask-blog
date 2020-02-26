4026e93e2df9bf445f3cffae72d8c09ef8fb3312
Amit Kumar <akamit21@outlook.com>
2020-02-27 01:45:52 +0530

    File changed

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchAllBlog } from "../../redux/actions/blogAction";
-import { Container, Card, Row, Col } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import Style from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 const Home = props => {
   useEffect(() => {
@@ -12,47 +13,29 @@ const Home = props => {
 
   return (
     <Container fluid className="section-padding">
-      <div className={`${Style.blogGrid}`}>
+      <div className={Style.blogGrid}>
         {blogList.map(blog => (
-          <figure className={`${Style.blogCard}`}>
+          <figure key={blog._id} className={Style.blogCard}>
             <Card className="shadow-lg">
-              <Card.Img variant="top" src={blog.image} />
+              <Card.Img variant="top" src={blog.image} alt="blog-image" />
 
               <Card.Body>
                 <span className="h6 fw-200 text-mute">
-                  by shufflehound November 23, 2016
+                  by {blog.username} on {blog.published_on}
                 </span>
-                <Card.Title>Trip that you’ll never ever forget</Card.Title>
-                <Card.Text>
-                  Quisque dictum eros nisl, a maximus massa accumsan non.
-                  Aliquam erat volutpat. Quisque at finibus dui. Praesent…
-                </Card.Text>
-                <div className="post-meta post-meta-two">
-                  <div className="sh-columns post-meta-comments">
-                    <span className="post-meta-categories">
-                      <i className="icon-tag"></i>
-                      <a
-                        href="https://jevelin.shufflehound.com/blog1/category/people/"
-                        rel="category tag"
-                      >
-                        People
-                      </a>
-                      ,{" "}
-                      <a
-                        href="https://jevelin.shufflehound.com/blog1/category/travel/"
-                        rel="category tag"
-                      >
-                        Travel
-                      </a>{" "}
-                    </span>
+                <Link
+                  to={`/blog/view/${blog.category_id}/${blog.user_id}/${blog._id}`}
+                >
+                  <Card.Title>{blog.blog_title}</Card.Title>
+                </Link>
+                <Card.Text>{blog.blog}</Card.Text>
+                <div className={Style.blogInfo}>
+                  <span className="fw-700">{blog.category_name}</span>
 
-                    <a
-                      href="https://jevelin.shufflehound.com/blog1/2016/11/23/trip-that-youll-never-forget/#comments"
-                      className="post-meta-comments"
-                    >
-                      <i className="icon icon-bubble"></i>0{" "}
-                    </a>
-                  </div>
+                  <a href="#" className={Style.blogComment}>
+                    <i className="fas fa-comments"></i>&nbsp;
+                    {blog.comment_count}
+                  </a>
                 </div>
               </Card.Body>
             </Card>
