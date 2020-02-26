90683d26bbd2018768dcad875f3a075dea7a8afa
Amit Kumar <akamit21@outlook.com>
2020-02-27 01:44:37 +0530

    File Modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,13 @@ import Header from "./component/Common/Header";
 import Menubar from "./component/Common/Menubar";
 import Home from "./component/Home/Home";
 import AddBlog from "./component/Blog/AddBlog";
+import Blog from "./component/Blog/Blog";
 
 import SignUp from "./component/Auth/SignUp";
 import Login from "./component/Auth/Login";
 
 import NotFound from "./component/Error/NotFound";
-
+import "./style.css";
 const App = () => {
   // const [state, setState] = useState({
   //   user: [],
@@ -32,9 +33,13 @@ const App = () => {
         <Route path="/" exact={true}>
           <Home />
         </Route>
-        <Route path="/blog/create" exact={true}>
+        <Route path="/blog/create">
           <AddBlog />
         </Route>
+        <Route
+          path="/blog/view/:category/:user/:id"
+          render={props => <Blog {...props} />}
+        />
         <Route path="/auth/signup">
           <SignUp />
         </Route>
